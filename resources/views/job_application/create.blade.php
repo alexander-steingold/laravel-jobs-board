3a47968aeb1de7d8af1c9d0eaf9496cb3c5c7198
Alexander Steingold <alex461976@gmail.com>
2023-07-26 09:36:09 +0300

    add jobs applications module, apply authorization policy, success and error messages, extend component with textarea option, upload cv pdf file and code refactoring

--- resources/views/job_application/create.blade.php
+++ resources/views/job_application/create.blade.php
@@ -13,16 +13,14 @@
         <form action="{{ route('job.application.store', $job) }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="mb-4">
-                <label for="expected_salary" class="mb-2 block text-sm font-medium text-slate-900">Expected
-                    Salary</label>
+                <x-label for="expected_salary" :required="true">Expected Salary</x-label>
                 <x-text-input name="expected_salary" type="number"/>
             </div>
             <div class="mb-4">
-                <label for="expected_salary" class="mb-2 block text-sm font-medium text-slate-900">Upload CV</label>
+                <x-label for="cv" :required="true">Upload CV</x-label>
                 <x-text-input name="cv" type="file"/>
             </div>
             <x-button class="w-full">Apply</x-button>
         </form>
     </x-card>
-
 </x-layout>
